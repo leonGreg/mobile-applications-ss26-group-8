7de9dc3b4909975281359d996bb4bed37512e129
Elsayed,Islam <islam.elsayed@study.thws.de>
2026-04-14 13:32:36 +0200

    team_member

---
+++ lib/models/team_member.dart
@@ -0,0 +1,17 @@
+class TeamMember {
+  final String name;
+  final String country;
+  final String role;
+  final String hobbies;
+  final String motto;
+  final String imagePath;
+
+  TeamMember({
+    required this.name,
+    required this.country,
+    required this.role,
+    required this.hobbies,
+    required this.motto,
+    required this.imagePath,
+  });
+}
