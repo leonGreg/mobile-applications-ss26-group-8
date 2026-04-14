b4dfc53013a08dc39a39c217e57a590f5209c67d
Elsayed,Islam <islam.elsayed@study.thws.de>
2026-04-14 13:19:30 +0200

    Add team data file

---
+++ lib/data/team_data.dart
@@ -0,0 +1,56 @@
+import '../models/team_member.dart';
+
+const String teamName = 'Team_8';
+const String teamDescription =
+    'We are a motivated international student team working on creative mobile app solutions with Flutter.';
+
+final List<TeamMember> teamMembers = [
+  TeamMember(
+    name: 'Islam',
+    country: 'Egypt',
+    role: 'Fullstack Developer',
+    hobbies: 'Coding, football, design',
+    motto: 'Keep building and keep learning.',
+    imagePath: 'assets/images/',
+  ),
+  TeamMember(
+    name: '',
+    country: '',
+    role: '',
+    hobbies: '',
+    motto: '',
+    imagePath: '',
+  ),
+  TeamMember(
+    name: '',
+    country: '',
+    role: '',
+    hobbies: '',
+    motto: '',
+    imagePath: '',
+  ),
+  TeamMember(
+    name: '',
+    country: '',
+    role: '',
+    hobbies: '',
+    motto: '',
+    imagePath: '',
+  ),
+  TeamMember(
+    name: '',
+    country: '',
+    role: '',
+    hobbies: '',
+    motto: '',
+    imagePath: '',
+  ),
+  TeamMember(
+    name: '',
+    country: '',
+    role: '',
+    hobbies: '',
+    motto: '',
+    imagePath: '',
+  ),
+];
