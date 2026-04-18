ba9606b3105bd6806f678b56cc19eda6ac870677
Elsayed,Islam <islam.elsayed@study.thws.de>
2026-04-18 09:55:43 +0200

    adding Dish model

---
+++ lib/models/dish.dart
@@ -0,0 +1,20 @@
+class Dish {
+  final String id;
+  final String name;
+  final String shortDescription;
+  final String longDescription;
+  final String category;
+  final double price;
+  final String imageUrl;
+
+  Dish({
+    required this.id,
+    required this.name,
+    required this.shortDescription,
+    required this.longDescription,
+    required this.category,
+    required this.price,
+    required this.imageUrl,
+  });
+  
+}
